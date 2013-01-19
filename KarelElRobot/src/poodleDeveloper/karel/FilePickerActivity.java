4b11062a998fb9ba6c676be2d74b10a3168fec6e
Poodle Developer <projects-danigarciaalva@hotmail.com>
2013-01-19 17:10:55 -0600

    Manejo de archivos en el editor, Listo!

--- KarelElRobot/src/poodleDeveloper/karel/FilePickerActivity.java
+++ KarelElRobot/src/poodleDeveloper/karel/FilePickerActivity.java
@@ -33,7 +33,6 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class FilePickerActivity extends ListActivity {
 	
