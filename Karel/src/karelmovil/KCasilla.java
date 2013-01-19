13611040ca11a41bd099006db669c0da49dbfc84
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2013-01-18 23:10:31 -0600

    corregido bug con las paredes

--- Karel/src/karelmovil/KCasilla.java
+++ Karel/src/karelmovil/KCasilla.java
@@ -1,23 +1,23 @@
 package karelmovil;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 
 public class KCasilla {
 	public int fila;
 	public int columna;
 	public int zumbadores;
-	public ArrayList<Integer> paredes;
+	public HashSet<Integer> paredes;
 	
 	public KCasilla(int fila, int columna){
 		this.fila = fila;
 		this.columna = columna;
 		this.zumbadores = 0;
-		this.paredes = new ArrayList<Integer>();
+		this.paredes = new HashSet<Integer>();
 	}
 	public KCasilla(KPosition posicion){
 		this.fila = posicion.fila;
 		this.columna = posicion.columna;
 		this.zumbadores = 0;
-		this.paredes = new ArrayList<Integer>();
+		this.paredes = new HashSet<Integer>();
 	}
 }
